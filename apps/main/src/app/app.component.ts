50f10922b2aa2436c00b11e925907f9aa36aeab4
Clement BEGNAUD <clement.begnaud@rca.fr>
2023-03-14 17:30:04 +0100

    initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cf-nx-test-multi-runners-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
